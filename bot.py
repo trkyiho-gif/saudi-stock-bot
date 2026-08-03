eac897869d1130051d318c0728bb52226f78483d
trkyiho-gif <trkyiho@gmail.com>
2026-08-03 12:05:09 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,4 +1,21 @@
+from flask import Flask
+import threading
 
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I am alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = threading.Thread(target=run)
+    t.daemon = True
+    t.start()
+
+keep_alive()
 
 
 
